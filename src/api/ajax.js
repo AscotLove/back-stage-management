d2608c8500da1bd25b50df671197e3236c1f55e2
SeasSystem <jxae093663@163.com>
2019-08-14 11:49:56 +0800

    '完成登录功能'

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,3 +1,30 @@
+/*
+* 封装了Axios请求
+* */
+
 import axios from 'axios';
 
-const base_url =
+console.log(process);
+const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://localhost:5000';
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 7000
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    const result = response.data;
+
+    if (result.status === 0) {
+      return result.data || {}
+    } else {
+      return Promise.reject(result.msg || '请求失败')
+    }
+  },
+  (error) => {
+    return Promise.reject('网络故障，请刷新')
+  }
+);
+
+export default axiosInstance;
--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axiosInstance from './ajax';
 
 
+// 定义登录请求方法
 export const reqLogin = (username, password) => axiosInstance.post('/login', {username, password});
