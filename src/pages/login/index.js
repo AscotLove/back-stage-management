5b214d466e26bfe683bd6a7c1136c3ad0de0cba0
SeasSystem <jxae093663@163.com>
2019-08-14 09:39:09 +0800

    添加了登录界面，和效验功能

---
+++ src/pages/login/index.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+
+import { Form, Input, Icon, Button, message } from 'antd';
+import axios from 'axios';
+import logo from './logo.png';
+
+import './index.less';
+const { Item } = Form;
+class Login extends Component {
+ validator = (rule, value, callback) => {
+   const name = rule.field === 'username' ? '用户名' : '密码';
+   const passwordReg = /^\w+$/;
+   if (!value) {
+     callback('输入内容不能为空');
+   } else if (value.length < 4) {
+     callback(`${name}c长度必须大于4位`);
+   } else if (value.length > 10) {
+     callback(`${name}长度必须小于10位`);
+   } else if (!passwordReg.test(value)) {
+     callback(`${name}只能包含英文、数字、下划线`);
+   };
+
+   callback();
+ };
+  login = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        axios.post('http://localhost:3000/login', values)
+          .then((res) => {
+            const result = res.data;
+            if (result.status === 0) {
+              message.success('登录成功', 3);
+            } else {
+              message.error(result.msg, 3);
+              this.props.form.resetFields(['password']);
+            }
+          })
+          .catch((error) => {
+            message.error('网络异常', 3);
+            this.props.form.resetFields(['password']);
+          })
+      }
+    })
+  };
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    return <div className="login">
+      <header className="login-header">
+        <img src={logo} alt="logo"/>
+        <h1>React项目: 后台管理系统</h1>
+      </header>
+      <section className="login-section">
+        <h2>用户登录</h2>
+        <Form onSubmit={this.login}>
+          <Item>
+            {
+              getFieldDecorator(
+                'username',
+                {
+                  rules: [
+                    {validator: this.validator}
+                  ]
+                }
+              )(
+                <Input prefix={<Icon type="user" placeholder="用户名"/>}/>
+              )
+            }
+
+        </Item>
+          <Item>
+            {
+              getFieldDecorator(
+                'password',
+                {
+                  rules: [
+                    {validator: this.validator}
+                  ]
+                }
+              )(
+                <Input prefix={<Icon type="lock" placeholder="密码"/>}/>
+              )
+            }
+
+          </Item>
+          <Item>
+            <Button type="primary" htmlType="submit" className="login-btn">提交</Button>
+          </Item>
+        </Form>
+      </section>
+    </div>
+  }
+}
+export default Form.create()(Login);
